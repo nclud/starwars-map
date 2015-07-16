9a1fdcfa8970dc335af3a9c91e5c020eb348d11d
Kyle Conrad <kyle@kyleconrad.com>
2015-07-16 17:17:38 -0400

    Updates to worker file

--- prod/js/workers/worker_json.js
+++ prod/js/workers/worker_json.js
@@ -9,29 +9,31 @@ function getLocalPlanetData() {
 	oboe( '/data/planets.json' )
 		.node('planets.*', function( planet ){
 			workerLocalPlanets.push( planet );
+			return oboe.drop;
 		})
-		.done(function( data ) {
-			// we got it
-		})
-		.fail(function() {
+		.done(function(){})
+		.fail(function(){
 			console.log('Local JSON error.');
 		});
 }
 
 function getRemotePlanetData() {
 	for ( i = 1; i < (pages + 1); i ++ ) {
-		console.log(i);
-
 		oboe( '//swapi.co/api/planets/?page=' + i )
 			.node('results.*', function( planet ){
 				workerRemotePlanets.push( planet );
+				return oboe.drop;
 			})
-			.done(function( data ) {
-				// we got it
-			})
-			.fail(function() {
+			.done(function(){})
+			.fail(function(){
 				console.log('Remote SWAPI JSON error.');
 			});
+	}
+}
+
+function planetRequestComplete() {
+	for ( i = 0; i < workerRemotePlanets.length; i ++ ) {
+
 	}
 }
 
