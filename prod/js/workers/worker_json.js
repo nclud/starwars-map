ca04681ff00af7880d1be57c920779fb84c4ec6f
Kyle Conrad <kyle@kyleconrad.com>
2015-07-16 18:14:51 -0400

    Added all data functions to worker

--- prod/js/workers/worker_json.js
+++ prod/js/workers/worker_json.js
@@ -5,6 +5,8 @@ var pages;
 var workerLocalPlanets = [],
 	workerRemotePlanets = [];
 
+var completed = [];
+
 function getLocalPlanetData() {
 	oboe( '/data/planets.json' )
 		.node('planets.*', function( planet ){
@@ -24,7 +26,13 @@ function getRemotePlanetData() {
 				workerRemotePlanets.push( planet );
 				return oboe.drop;
 			})
-			.done(function(){})
+			.done(function(){
+				completed.push(0);
+
+				if ( completed.length == pages ) {
+					planetRequestComplete();
+				}
+			})
 			.fail(function(){
 				console.log('Remote SWAPI JSON error.');
 			});
@@ -33,8 +41,38 @@ function getRemotePlanetData() {
 
 function planetRequestComplete() {
 	for ( i = 0; i < workerRemotePlanets.length; i ++ ) {
+		// console.log(i);
 
+		// GIVE GENERAL DIAMETER & ROTATION IF MISSING
+		if ( workerRemotePlanets[i].diameter === 'unknown' || workerRemotePlanets[i].diameter === '0' ) {
+			workerRemotePlanets[i].diameter = 10000;
+		}
+		if ( workerRemotePlanets[i].rotation_period === 'unknown' || workerRemotePlanets[i].rotation_period === '0' ) {
+			workerRemotePlanets[i].rotation_period = 24;
+		}
+
+		// CHANGE DIAMETER & ORBIT TO NUMBERS FROM STRINGS
+		var numberDiameter = parseInt( workerRemotePlanets[i].diameter ),
+			numberOrbit = parseInt(workerRemotePlanets[i].rotation_period);
+
+		workerRemotePlanets[i].diameter = numberDiameter;
+		workerRemotePlanets[i].rotation_period = numberOrbit;
+
+		// SHRINK LARGE PLANETS
+		if ( workerRemotePlanets[i].diameter > 100000 ) {
+			workerRemotePlanets[i].diameter = numberDiameter / 5;
+		}
+
+		// ADD X & Z POSITION TO PLANET DATA
+		for ( x = 0; x < workerLocalPlanets.length; x ++ ) {
+			if ( workerLocalPlanets[x].name == workerRemotePlanets[i].name ) {
+				workerRemotePlanets[i].xpos = workerLocalPlanets[x].xpos;
+				workerRemotePlanets[i].zpos = workerLocalPlanets[x].zpos;
+			}
+		}
 	}
+
+	console.log(workerRemotePlanets);
 }
 
 
