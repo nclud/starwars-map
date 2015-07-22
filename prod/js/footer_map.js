d9b382b4e68116f26ee21e5d574b244c1b982101
Kyle Conrad <kyle@kyleconrad.com>
2015-07-22 12:33:34 -0400

    Moved stats to top right

--- prod/js/footer_map.js
+++ prod/js/footer_map.js
@@ -158,6 +158,7 @@ $(document).ready(function() {
 		stats = new Stats();
 		stats.domElement.style.position = 'absolute';
 		stats.domElement.style.top = '0px';
+		stats.domElement.style.right = '0px';
 		container.appendChild( stats.domElement );
 
 
