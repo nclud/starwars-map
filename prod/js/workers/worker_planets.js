d6ca16075647ebf9e3f686ee7021b6bc97f484e9
Kyle Conrad <kyle@kyleconrad.com>
2015-07-24 12:03:15 -0400

    Tweaks to text and workers

--- prod/js/workers/worker_planets.js
+++ prod/js/workers/worker_planets.js
@@ -6,10 +6,6 @@ var workerLocalPlanets = [],
 	workerFilms = [],
 	workerRemotePlanets = [];
 
-var localPlanetSwitch = false,
-	remoteFilmSwitch = false,
-	remotePlanetSwitch = false;
-
 var completed = [];
 
 function getLocalPlanetData() {
@@ -18,11 +14,7 @@ function getLocalPlanetData() {
 			workerLocalPlanets.push( planet );
 			return oboe.drop;
 		})
-		.done(function(){
-			localPlanetSwitch = true;
-
-			planetRequestComplete();
-		})
+		.done(function(){})
 		.fail(function(){
 			console.log('Local JSON error.');
 		});
@@ -39,11 +31,7 @@ function getFilmData() {
 
 			return oboe.drop;
 		})
-		.done(function(){
-			remoteFilmSwitch = true;
-
-			planetRequestComplete();
-		})
+		.done(function(){})
 		.fail(function(){
 			console.log('Remote SWAPI JSON error with films.');
 		});
@@ -72,71 +60,43 @@ function getRemotePlanetData() {
 }
 
 function planetRequestComplete() {
-	if ( localPlanetSwitch && remoteFilmSwitch && remotePlanetSwitch ) {
-		for ( i = 0; i < workerRemotePlanets.length; i ++ ) {
-			// console.log(i);
+	for ( i = 0; i < workerRemotePlanets.length; i ++ ) {
+		// console.log(i);
 
-			// GIVE GENERAL DIAMETER & ROTATION IF MISSING
-			if ( workerRemotePlanets[i].diameter === 'unknown' || workerRemotePlanets[i].diameter === '0' ) {
-				workerRemotePlanets[i].diameter = 10000;
-			}
-			if ( workerRemotePlanets[i].rotation_period === 'unknown' || workerRemotePlanets[i].rotation_period === '0' ) {
-				workerRemotePlanets[i].rotation_period = 24;
-			}
-
-			// CHANGE DIAMETER & ORBIT TO NUMBERS FROM STRINGS
-			var numberDiameter = parseInt( workerRemotePlanets[i].diameter ),
-				numberOrbit = parseInt(workerRemotePlanets[i].rotation_period);
-
-			workerRemotePlanets[i].diameter = numberDiameter;
-			workerRemotePlanets[i].rotation_period = numberOrbit;
-
-			// SHRINK LARGE PLANETS
-			if ( workerRemotePlanets[i].diameter > 100000 ) {
-				workerRemotePlanets[i].diameter = numberDiameter / 5;
-			}
-
-			// MATCHING PLANET DATA SETS
-			for ( x = 0; x < workerLocalPlanets.length; x ++ ) {
-				if ( workerLocalPlanets[x].name == workerRemotePlanets[i].name ) {
-					// ADD X & Z POSITION TO PLANET DATA
-					workerRemotePlanets[i].xpos = workerLocalPlanets[x].xpos;
-					workerRemotePlanets[i].zpos = workerLocalPlanets[x].zpos;
-
-					// ADD TEXTURES
-					workerRemotePlanets[i].texture = workerLocalPlanets[x].terrain
-				}
-			}
-
-			// MATCHING FILM DATA SETS
-			for ( y = 0; y < workerRemotePlanets[i].films.length; y ++ ) {
-				// console.log( workerRemotePlanets[i].name );
-				for ( z = 0; z < workerFilms.length; z ++ ) {
-					if ( workerFilms[z].url  == workerRemotePlanets[i].films[y] ) {
-						// console.log( workerRemotePlanets[i].name, workerFilms[z].url, workerRemotePlanets[i].films[y] );
-
-						var film_titles = {},
-							episodes = {};
-
-						workerRemotePlanets[i].film_titles = film_titles;
-						workerRemotePlanets[i].episodes = episodes;
-						// console.log(film_titles);
-						workerRemotePlanets[i].film_titles.film_title = workerFilms[z].title;
-						workerRemotePlanets[i].episodes.episode = workerFilms[z].episode_id;
-					}
-				}
-				// if ( workerFilms[y].url == workerRemotePlanets[i].films ) {
-				// 	// console.log( workerFilms[y].url );
-				// 	// workerRemotePlanets[i].film_title = workerFilms[y].title;
-				// 	// workerRemotePlanets[i].episode_id = workerFilms[y].episode_id;
-				// 	console.log( workerRemotePlanets[i].name );
-				// }
-			}
+		// GIVE GENERAL DIAMETER & ROTATION IF MISSING
+		if ( workerRemotePlanets[i].diameter === 'unknown' || workerRemotePlanets[i].diameter === '0' ) {
+			workerRemotePlanets[i].diameter = 10000;
+		}
+		if ( workerRemotePlanets[i].rotation_period === 'unknown' || workerRemotePlanets[i].rotation_period === '0' ) {
+			workerRemotePlanets[i].rotation_period = 24;
 		}
 
-		console.log( workerRemotePlanets );
-		// postMessage( workerRemotePlanets );
+		// CHANGE DIAMETER & ORBIT TO NUMBERS FROM STRINGS
+		var numberDiameter = parseInt( workerRemotePlanets[i].diameter ),
+			numberOrbit = parseInt(workerRemotePlanets[i].rotation_period);
+
+		workerRemotePlanets[i].diameter = numberDiameter;
+		workerRemotePlanets[i].rotation_period = numberOrbit;
+
+		// SHRINK LARGE PLANETS
+		if ( workerRemotePlanets[i].diameter > 100000 ) {
+			workerRemotePlanets[i].diameter = numberDiameter / 5;
+		}
+
+		// MATCHING PLANET DATA SETS
+		for ( x = 0; x < workerLocalPlanets.length; x ++ ) {
+			if ( workerLocalPlanets[x].name == workerRemotePlanets[i].name ) {
+				// ADD X & Z POSITION TO PLANET DATA
+				workerRemotePlanets[i].xpos = workerLocalPlanets[x].xpos;
+				workerRemotePlanets[i].zpos = workerLocalPlanets[x].zpos;
+
+				// ADD TEXTURES
+				workerRemotePlanets[i].texture = workerLocalPlanets[x].terrain
+			}
+		}
 	}
+
+	postMessage( workerRemotePlanets );
 }
 
 
