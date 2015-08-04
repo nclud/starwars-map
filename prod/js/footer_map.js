dbef9721ed01393fcf6594b71a554eca52f728f6
Kyle Conrad <kyle@kyleconrad.com>
2015-08-04 13:40:01 -0400

    Moving stats

--- prod/js/footer_map.js
+++ prod/js/footer_map.js
@@ -167,7 +167,7 @@ $(document).ready(function() {
 		// STATS
 		stats = new Stats();
 		stats.domElement.style.position = 'absolute';
-		stats.domElement.style.top = '0px';
+		stats.domElement.style.bottom = '0px';
 		stats.domElement.style.right = '0px';
 		container.appendChild( stats.domElement );
 
